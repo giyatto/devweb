5a4db54f95a182e4b26d03dd109620d80759b6f9
giyatto <bluem31@gmail.com>
2015-04-16 13:07:24 +0900

    Dependency Injection 적용. (테스트)

--- test/org/nhn/next/di/MessageRenderer.java
+++ test/org/nhn/next/di/MessageRenderer.java
@@ -1,13 +1,24 @@
 package org.nhn.next.di;
 
 public class MessageRenderer {
+	
+	private MessageProvider messageProvider;
+	
+	public void setMessageProvider(MessageProvider messageProvider) {
+		this.messageProvider = messageProvider;
+	}
+	
 	public void render(){
-		MessageProvider mp = new HelloWorldMessageProvider();
-		System.out.println(mp.getMessage());
+		System.out.println(messageProvider.getMessage());
 	}
 	
 	public static void main(String[] args){
 		MessageRenderer render = new MessageRenderer();
+		
+		render.setMessageProvider(new HelloWorldMessageProvider());
+		render.render();
+		
+		render.setMessageProvider(new HiWorldMessageProvider());
 		render.render();
 	}
 }
